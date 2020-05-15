25d526ab8e68999e2996ebc43ed87e4cebf8d9c6
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-05-15 16:08:36 +0200

    fix v0.3.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/keyko-io/nevermined-contracts',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
 
