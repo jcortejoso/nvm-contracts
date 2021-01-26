97d21cae9b9c5fb0498fd15db8492dfabe775fbb
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-01-26 10:28:25 +0100

    Create docker image with contracts pre-conmpiled

---
+++ keeper-spree.Dockerfile
@@ -0,0 +1,40 @@
+FROM openethereum/openethereum:v3.1.0 as openethereum
+
+COPY networks/spree/config /home/openethereum/config
+COPY networks/spree/authorities/validator0.json /home/openethereum/.local/keys/spree/validator.json
+COPY networks/spree/keys /home/openethereum/.local/keys/spree
+COPY networks/spree/authorities/validator0.pwd /home/openethereum/validator.pwd
+
+
+
+FROM node:10-alpine
+LABEL maintainer="Keyko <root@keyko.io>"
+
+RUN apk add --no-cache --update\
+      bash\
+      g++\
+      gcc\
+      git\
+      krb5-dev\
+      krb5-libs\
+      krb5\
+      make\
+      python\
+      curl
+
+COPY --from=openethereum /home/openethereum /home/openethereum
+
+COPY . /nevermined-contracts
+WORKDIR /nevermined-contracts
+
+RUN yarn
+
+ENV MNEMONIC "taxi music thumb unique chat sand crew more leg another off lamp"
+ENV DEPLOY_CONTRACTS true
+ENV LOCAL_CONTRACTS true
+ENV REUSE_DATABASE false
+ENV NETWORK_NAME spree
+ENV KEEPER_RPC_HOST localhost
+ENV KEEPER_RPC_PORT 8545
+
+RUN /nevermined-contracts/scripts/keeper2.sh
---
+++ scripts/keeper2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -em
+
+# default to false in case it is not set
+DEPLOY_CONTRACTS="${DEPLOY_CONTRACTS:-false}"
+
+echo "deploy contracts is ${DEPLOY_CONTRACTS}"
+
+if [ "${DEPLOY_CONTRACTS}" = "true" ]
+then
+    /home/openethereum/openethereum \
+      --config /home/openethereum/config/config.toml \
+      --db-path /home/openethereum/chains \
+      --keys-path /home/openethereum/.local/keys \
+      --base-path /home/openethereum/base \
+      --min-gas-price 0 \
+      --jsonrpc-cors all \
+      --jsonrpc-interface all \
+      --jsonrpc-hosts all \
+      --jsonrpc-apis all \
+      --unsafe-expose \
+      --no-warp \
+      --unlock 0x00bd138abd70e2f00903268f3db08f2d25677c9e \
+      --node-key 0xb3244c104fb56d28d3979f6cd14a8b5cf5b109171d293f4454c97c173a9f9374 &
+
+      sleep 5
+      curl --data '{"method":"web3_clientVersion","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545
+
+    # remove ready flag if we deploy contracts
+    rm -f /nevermined-contracts/artifacts/ready
+
+    npm run clean
+    npm run compile
+    export NETWORK="${NETWORK_NAME:-development}"
+
+
+
+
+    npm run deploy:${NETWORK}
+
+    # set flag to indicate contracts are ready
+    touch /nevermined-contracts/artifacts/ready
+fi
+
+# Fix file permissions
+EXECUTION_UID=$(id -u)
+EXECUTION_GID=$(id -g)
+USER_ID=${LOCAL_USER_ID:-$EXECUTION_UID}
+GROUP_ID=${LOCAL_GROUP_ID:-$EXECUTION_GID}
+chown -R $USER_ID:$GROUP_ID /nevermined-contracts/artifacts
