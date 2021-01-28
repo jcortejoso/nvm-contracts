ec0945f860c827061731c0bd9d942ed568ddf9f2
Aitor <1726644+aaitor@users.noreply.github.com>
2021-01-28 17:02:56 +0100

    workaround to fix the problems with web3j-cli, fixes https://github.com/nevermined-io/sdk-java/issues/39

--- scripts/maven.sh
+++ scripts/maven.sh
@@ -5,7 +5,7 @@ export LC_ALL=en_US.UTF-8
 ## Generating web3j stubs
 
 shopt -s nullglob # Avoid literal evaluation if not files
-mkdir ./tmp/
+mkdir -p ./tmp/
 
 #for file in artifacts/*.development.json
 for file in build/contracts/*.json
@@ -15,7 +15,7 @@ do
 
     cp $file ./tmp/${tmpFile}
 
-    web3j truffle generate --javaTypes ./tmp/${tmpFile} -o src/main/java -p io.keyko.nevermined.contracts
+    web3j truffle generate ./tmp/${tmpFile} -o src/main/java -p io.keyko.nevermined.contracts
 done
 
 rm -rf ./tmp/
