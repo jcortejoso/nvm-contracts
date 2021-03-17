73877818081d5b144a69db9c01ca7f3e1c16525b
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-03-17 12:39:52 +0100

    fix test

--- scripts/deploy/truffle-wrapper/deploy/setupContracts.js
+++ scripts/deploy/truffle-wrapper/deploy/setupContracts.js
@@ -236,21 +236,27 @@ async function setupContracts({
         })
     }
 
-    if (addressBook.TransferNFTCondition) {
+    if (addressBook.TransferNFTCondition && addressBook.DIDRegistry) {
         const DIDRegistry = artifacts.require('DIDRegistry')
         const DIDRegistryInstance =
             await DIDRegistry.at(addressBook.DIDRegistry)
 
-        //        const didRegistryOwner = await DIDRegistryInstance.owner()
-        //        console.log('didRegistryOwner : ' + didRegistryOwner)
         console.log('TransferNFTCondition : ' + addressBook.TransferNFTCondition)
         await DIDRegistryInstance.setProxyApproval(
             addressBook.TransferNFTCondition, true, { from: roles.deployer })
+    }
 
-        await DIDRegistryInstance.transferOwnership(
-            roles.ownerWallet,
-            { from: roles.deployer }
-        )
+    if(addressBook.DIDRegistry) {
+        const DIDRegistry = artifacts.require('DIDRegistry')
+        const DIDRegistryInstance =
+            await DIDRegistry.at(addressBook.DIDRegistry)
+
+        await transferOwnership({
+            ContractInstance: DIDRegistryInstance,
+            name: DIDRegistry.contractName,
+            roles,
+            verbose
+        })
     }
 
     if (addressBook.NeverminedToken) {
