54871c378992ad6b32ec22f95527fbfad86fc180
Aitor <1726644+aaitor@users.noreply.github.com>
2021-04-29 15:56:17 +0200

    `_providers` documentation

--- contracts/registry/DIDFactory.sol
+++ contracts/registry/DIDFactory.sol
@@ -192,7 +192,7 @@ contract DIDFactory is OwnableUpgradeable, ProvenanceRegistry {
      * @param _checksum includes a one-way HASH calculated using the DDO content.
      * @param _providers list of addresses that can act as an asset provider     
      * @param _url refers to the url resolving the DID into a DID Document (DDO), limited to 2048 bytes.
-     * @param _providers ???
+     * @param _providers list of DID providers addresses
      * @param _activityId refers to activity
      * @param _attributes refers to the provenance attributes     
      * @return size refers to the size of the registry after the register action.
