ed216d2367421e66b96c3562b93b8bd13af41a15
Sami Mäkelä <mrsmkl@gmail.com>
2021-07-01 14:58:13 +0300

    added holder condition test for ERC721

---
+++ contracts/test/TestERC721.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.6.12;
+// Copyright 2020 Keyko GmbH.
+// SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+// Code is Apache-2.0 and docs are CC-BY-4.0
+
+import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+
+contract TestERC721 is ERC721Upgradeable {
+
+    function initialize() public initializer {
+        __ERC721_init_unchained('Test', 'TEST');
+    }
+
+    function mint(uint256 id) public {
+        _mint(msg.sender, id);
+    }
+
+}
