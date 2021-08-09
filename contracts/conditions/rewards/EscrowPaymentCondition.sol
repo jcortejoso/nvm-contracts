ffd1f6d8119f7f5d6eabd93d6be2c9543b5b4177
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-08-09 16:15:39 +0200

    lint free

--- contracts/conditions/rewards/EscrowPaymentCondition.sol
+++ contracts/conditions/rewards/EscrowPaymentCondition.sol
@@ -287,10 +287,13 @@ contract EscrowPaymentCondition is Reward, Common, ReentrancyGuardUpgradeable {
                 _receivers[i] != address(this),
                 'Escrow contract can not be a receiver'
             );
+            
             require(
                 address(this).balance >= _amounts[i],
                 'Contract balance too low'
             );
+            
+            // solhint-disable-next-line
             (bool sent,) = _receivers[i].call{value: _amounts[i]}('');
             require(sent, 'Failed to send Ether');
         }
