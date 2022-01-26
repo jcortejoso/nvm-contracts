2a28efe4131e08a2de5dc84c0a9b7d001446fc0b
Sami Mäkelä <mrsmkl@gmail.com>
2022-01-26 15:56:57 +0200

    starting writing tests

---
+++ test/int/nft/NFTAccessProofAgreement.Test.js
@@ -0,0 +1,360 @@
+/* eslint-env mocha */
+/* eslint-disable no-console */
+/* global artifacts, contract, describe, it, expect */
+
+const chai = require('chai')
+const { assert } = chai
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+
+const NFTAccessProofTemplate = artifacts.require('NFTAccessProofTemplate')
+const NFTHolderCondition = artifacts.require('NFTHolderCondition')
+
+const constants = require('../../helpers/constants.js')
+const deployConditions = require('../../helpers/deployConditions.js')
+const deployManagers = require('../../helpers/deployManagers.js')
+const { getBalance } = require('../../helpers/getBalance.js')
+const increaseTime = require('../../helpers/increaseTime.js')
+const testUtils = require('../../helpers/utils')
+const mimcdecrypt = require('../../helpers/mimcdecrypt').decrypt
+
+const poseidon = require('circomlib').poseidon
+const babyJub = require('circomlib').babyJub
+const mimcjs = require('circomlib').mimcsponge
+const ZqField = require('ffjavascript').ZqField
+const Scalar = require('ffjavascript').Scalar
+const F = new ZqField(Scalar.fromString('21888242871839275222246405745257275088548364400416034343698204186575808495617'))
+const snarkjs = require('snarkjs')
+const { unstringifyBigInts } = require('ffjavascript').utils
+
+contract('NFT Access Proof Template integration test', (accounts) => {
+    const web3 = global.web3
+    const didSeed = testUtils.generateId()
+    const checksum = testUtils.generateId()
+    const url = 'https://raw.githubusercontent.com/nevermined-io/assets/main/images/logo/banner_logo.png'
+    const royalties = 10 // 10% of royalties in the secondary market
+    const cappedAmount = 5
+    let token,
+        didRegistry,
+        nft,
+        agreementStoreManager,
+        conditionStoreManager,
+        templateStoreManager,
+        nftAccessTemplate,
+        accessProofCondition,
+        lockPaymentCondition,
+        escrowPaymentCondition
+    const [
+        owner,
+        deployer,
+        artist,
+        collector1,
+        collector2,
+        gallery,
+        market,
+        someone
+    ] = accounts
+    async function setupTest() {
+        ({
+            token,
+            didRegistry,
+            nft,
+            agreementStoreManager,
+            conditionStoreManager,
+            templateStoreManager
+        } = await deployManagers(
+            deployer,
+            owner
+        ));
+
+        ({
+            accessProofCondition,
+            lockPaymentCondition,
+            escrowPaymentCondition
+        } = await deployConditions(
+            deployer,
+            owner,
+            agreementStoreManager,
+            conditionStoreManager,
+            didRegistry,
+            token
+        ))
+        nftHolderCondition = await NFTHolderCondition.new({ from: deployer })
+        await nftHolderCondition.initialize(
+            owner,
+            conditionStoreManager.address,
+            nft.address,
+            { from: deployer }
+        )
+
+        nftAccessTemplate = await NFTAccessProofTemplate.new()
+        await nftAccessTemplate.methods['initialize(address,address,address,address)'](
+            owner,
+            agreementStoreManager.address,
+            nftHolderCondition.address,
+            accessProofCondition.address,
+            { from: deployer }
+        )
+
+        // propose and approve template
+        const templateId = nftAccessTemplate.address
+        await templateStoreManager.proposeTemplate(templateId)
+        await templateStoreManager.approveTemplate(templateId, { from: owner })
+
+        return {
+            templateId,
+            owner
+        }
+    }
+
+    async function prepareAgreement({
+        agreementId = testUtils.generateId(),
+        sender = accounts[0],
+        receivers = [accounts[2], accounts[3]],
+        escrowAmounts = [11, 4],
+        timeLockAccess = 0,
+        timeOutAccess = 0,
+        didSeed = testUtils.generateId(),
+        url = constants.registry.url,
+        checksum = constants.bytes32.one
+    } = {}) {
+        const orig1 = 222n
+        const orig2 = 333n
+        const origHash = poseidon([orig1, orig2])
+
+        const did = await didRegistry.hashDID(didSeed, receivers[0])
+
+        const buyerK = 123
+        const providerK = 234
+        const buyerPub = babyJub.mulPointEscalar(babyJub.Base8, F.e(buyerK))
+        const providerPub = babyJub.mulPointEscalar(babyJub.Base8, F.e(providerK))
+
+        const k = babyJub.mulPointEscalar(buyerPub, F.e(providerK))
+
+        const cipher = mimcjs.hash(orig1, orig2, k[0])
+
+        const snarkParams = {
+            buyer_x: buyerPub[0],
+            buyer_y: buyerPub[1],
+            provider_x: providerPub[0],
+            provider_y: providerPub[1],
+            xL_in: orig1,
+            xR_in: orig2,
+            cipher_xL_in: cipher.xL,
+            cipher_xR_in: cipher.xR,
+            provider_k: providerK,
+            hash_plain: origHash
+        }
+
+        // console.log(snark_params)
+
+        const { proof } = await snarkjs.plonk.fullProve(
+            snarkParams,
+            'circuits/keytransfer.wasm',
+            'circuits/keytransfer.zkey'
+        )
+
+        const signals = [
+            buyerPub[0],
+            buyerPub[1],
+            providerPub[0],
+            providerPub[1],
+            cipher.xL,
+            cipher.xR,
+            origHash
+        ]
+
+        const proofSolidity = (await snarkjs.plonk.exportSolidityCallData(unstringifyBigInts(proof), signals))
+
+        const proofData = proofSolidity.split(',')[0]
+
+        // construct agreement
+        const conditionIdNFTHolder = await nftHolderCondition.generateId(agreementAccessId,
+            await nftHolderCondition.hashValues(did, receiver, 1))
+        const conditionIdAccess = await accessProofCondition.generateId(agreementId,
+            await accessProofCondition.hashValues(origHash, buyerPub, providerPub))
+
+        const agreement = {
+            did: did,
+            conditionIds: [
+                conditionIdNFTHolder,
+                conditionIdAccess
+            ],
+            timeLocks: [0, 0],
+            timeOuts: [0, 0],
+            accessConsumer: receiver
+        }
+        const data = {
+            origHash,
+            buyerPub,
+            providerPub,
+            cipher: [cipher.xL, cipher.xR],
+            proof: proofData
+        }
+        return {
+            agreementId,
+            did,
+            data,
+            didSeed,
+            agreement,
+            sender,
+            receivers,
+            escrowAmounts,
+            timeLockAccess,
+            timeOutAccess,
+            checksum,
+            url,
+            buyerK,
+            providerPub,
+            origHash
+        }
+    }
+
+    describe.only('As an artist I want to register a new artwork', () => {
+        it('I want to register a new artwork and tokenize (via NFT). I want to get 10% of royalties', async () => {
+            await setupTest()
+
+            did = await didRegistry.hashDID(didSeed, artist)
+
+            await didRegistry.registerMintableDID(
+                didSeed, checksum, [], url, cappedAmount, royalties, constants.activities.GENERATED, '', { from: artist })
+            await didRegistry.mint(did, 5, { from: artist })
+
+            const balance = await nft.balanceOf(artist, did)
+            assert.strictEqual(5, balance.toNumber())
+        })
+    })
+
+    describe('create and fulfill access agreement', function() {
+        this.timeout(100000)
+        it('should create access agreement', async () => {
+            // prepare: escrow agreement
+            const { agreementId, data, did, didSeed, agreement, sender, receivers, escrowAmounts, checksum, url, buyerK, providerPub, origHash } = await prepareAgreement()
+            const totalAmount = escrowAmounts[0] + escrowAmounts[1]
+            const receiver = receivers[0]
+            // register DID
+            await didRegistry.registerAttribute(didSeed, checksum, [], url, { from: receiver })
+
+            // create agreement
+            await accessTemplate.createAgreement(agreementId, ...Object.values(agreement))
+
+            // check state of agreement and conditions
+            expect((await agreementStoreManager.getAgreement(agreementId)).did)
+                .to.equal(did)
+
+            const conditionTypes = await accessTemplate.getConditionTypes()
+            let storedCondition
+            agreement.conditionIds.forEach(async (conditionId, i) => {
+                storedCondition = await conditionStoreManager.getCondition(conditionId)
+                expect(storedCondition.typeRef).to.equal(conditionTypes[i])
+                expect(storedCondition.state.toNumber()).to.equal(constants.condition.state.unfulfilled)
+            })
+
+            // fill up wallet
+            await token.mint(sender, totalAmount, { from: owner })
+
+            assert.strictEqual(await getBalance(token, sender), totalAmount)
+            assert.strictEqual(await getBalance(token, lockPaymentCondition.address), 0)
+            assert.strictEqual(await getBalance(token, escrowPaymentCondition.address), 0)
+            assert.strictEqual(await getBalance(token, receiver), 0)
+
+            // fulfill lock reward
+            await token.approve(lockPaymentCondition.address, totalAmount, { from: sender })
+            await lockPaymentCondition.fulfill(agreementId, did, escrowPaymentCondition.address, token.address, escrowAmounts, receivers, { from: sender })
+
+            assert.strictEqual(await getBalance(token, sender), 0)
+            assert.strictEqual(await getBalance(token, lockPaymentCondition.address), 0)
+            assert.strictEqual(await getBalance(token, escrowPaymentCondition.address), totalAmount)
+            assert.strictEqual(await getBalance(token, receiver), 0)
+
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[1])).toNumber(),
+                constants.condition.state.fulfilled)
+
+            // fulfill access
+            // await disputeManager.setAccepted(...Object.values(data))
+            await accessProofCondition.fulfill(agreementId, ...Object.values(data), { from: receiver })
+
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[0])).toNumber(),
+                constants.condition.state.fulfilled)
+
+            // get reward
+            await escrowPaymentCondition.fulfill(agreementId, did, escrowAmounts, receivers, escrowPaymentCondition.address, token.address, agreement.conditionIds[1], agreement.conditionIds[0], { from: receiver })
+
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[2])).toNumber(),
+                constants.condition.state.fulfilled
+            )
+
+            assert.strictEqual(await getBalance(token, sender), 0)
+            assert.strictEqual(await getBalance(token, lockPaymentCondition.address), 0)
+            assert.strictEqual(await getBalance(token, escrowPaymentCondition.address), 0)
+            assert.strictEqual(await getBalance(token, receivers[0]), escrowAmounts[0])
+            assert.strictEqual(await getBalance(token, receivers[1]), escrowAmounts[1])
+
+            // make sure decryption works
+            const ev = await accessProofCondition.getPastEvents('Fulfilled', { fromBlock: 0, toBlock: 'latest', filter: { _agreementId: agreementId } })
+            const [cipherL, cipherR] = ev[0].returnValues._cipher
+            const k2 = babyJub.mulPointEscalar(providerPub, F.e(buyerK))
+
+            const plain = mimcdecrypt(cipherL, cipherR, k2[0])
+            assert.strictEqual(origHash, poseidon([plain.xL, plain.xR]))
+        })
+
+        it('should create escrow agreement and abort after timeout', async () => {
+            const { owner } = await setupTest()
+
+            // prepare: escrow agreement
+            const { agreementId, data, did, didSeed, agreement, sender, receivers, escrowAmounts, checksum, url, timeOutAccess } = await prepareEscrowAgreementMultipleEscrow({ timeOutAccess: 10 })
+            const totalAmount = escrowAmounts[0] + escrowAmounts[1]
+            const receiver = receivers[0]
+
+            // register DID
+            await didRegistry.registerAttribute(didSeed, checksum, [], url, { from: receiver })
+
+            // create agreement
+            await accessTemplate.createAgreement(agreementId, ...Object.values(agreement))
+
+            // fill up wallet
+            await token.mint(sender, totalAmount, { from: owner })
+
+            // fulfill lock reward
+            await token.approve(lockPaymentCondition.address, totalAmount, { from: sender })
+            await lockPaymentCondition.fulfill(agreementId, did, escrowPaymentCondition.address, token.address, escrowAmounts, receivers, { from: sender })
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[1])).toNumber(),
+                constants.condition.state.fulfilled)
+
+            // No update since access is not fulfilled yet
+            // refund
+            const result = await escrowPaymentCondition.fulfill(agreementId, did, escrowAmounts, receivers, escrowPaymentCondition.address, token.address, agreement.conditionIds[1], agreement.conditionIds[0], { from: receiver })
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[2])).toNumber(),
+                constants.condition.state.unfulfilled
+            )
+            assert.strictEqual(result.logs.length, 0)
+
+            // wait: for time out
+            await increaseTime.mineBlocks(web3, timeOutAccess)
+
+            // abort: fulfill access after timeout
+            await accessProofCondition.fulfill(agreementId, ...Object.values(data), { from: receiver })
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[0])).toNumber(),
+                constants.condition.state.aborted)
+
+            // refund
+            await escrowPaymentCondition.fulfill(agreementId, did, escrowAmounts, receivers, escrowPaymentCondition.address, token.address, agreement.conditionIds[1], agreement.conditionIds[0], { from: sender })
+            assert.strictEqual(
+                (await conditionStoreManager.getConditionState(agreement.conditionIds[2])).toNumber(),
+                constants.condition.state.fulfilled
+            )
+            assert.strictEqual(await getBalance(token, receivers[0]), 0)
+            assert.strictEqual(await getBalance(token, receivers[1]), 0)
+            assert.strictEqual(await getBalance(token, escrowPaymentCondition.address), 0)
+            assert.strictEqual(await getBalance(token, sender), totalAmount)
+        })
+    })
+
+})
