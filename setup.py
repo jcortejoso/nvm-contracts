ccdc3472937dba2ceb566a23008d22e79f578715
Aitor <1726644+aaitor@users.noreply.github.com>
2022-02-08 14:27:36 +0100

    bumping to v1.3.5

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts',
-    version='1.3.4',
+    version='1.3.5',
     zip_safe=False,
 )
