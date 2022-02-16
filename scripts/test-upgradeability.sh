ff0b8274a1122b1b8b89a15e92ecc10e0d0ff116
Sami Mäkelä <mrsmkl@gmail.com>
2022-02-16 14:19:49 +0200

    handle errors on script

--- scripts/test-upgradeability.sh
+++ scripts/test-upgradeability.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+export BASE=master
 export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 rm -f .openzeppelin/unknown-31337.json
-git checkout master
+git checkout $BASE
 yarn
 yarn compile
 
 npx hardhat node --port 18545 &
 
-npx hardhat run ./scripts/deploy/truffle-wrapper/deployContractsWrapper.js --network external
+sleep 10
+
+npx hardhat run ./scripts/deploy/truffle-wrapper/deployContractsWrapper.js --network external || exit 1
 
 git checkout $BRANCH
 yarn
 
-npx hardhat run ./scripts/deploy/truffle-wrapper/upgradeContractsWrapper.js --network external
+npx hardhat run ./scripts/deploy/truffle-wrapper/upgradeContractsWrapper.js --network external || exit 1
