a26191b029d965e346beb9a373a716ba11d91280
Sami Mäkelä <mrsmkl@gmail.com>
2022-05-17 15:54:03 +0300

    remove from npm

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,4 +16,3 @@ mv keytransfer_js/keytransfer.wasm circuits
 yarn run snarkjs plonk setup circuits/keytransfer.r1cs powersOfTau28_hez_final_14.ptau circuits/keytransfer.zkey
 yarn run snarkjs zkey export verificationkey circuits/keytransfer.zkey circuits/verification_key.json
 yarn run snarkjs zkey export solidityverifier circuits/keytransfer.zkey contracts/verifier.sol
-# sed -i 's/pragma solidity >=0.7.0 <0.9.0/pragma solidity 0.6.12/' contracts/verifier.sol
