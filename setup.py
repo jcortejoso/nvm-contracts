8ab3eac818bd4cb9f023c3b32aa942e62302ea27
Aitor <1726644+aaitor@users.noreply.github.com>
2022-05-24 10:55:59 +0200

    Adding v2.0.0-rc3 Changelog updates

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts',
-    version='2.0.0-rc2',
+    version='2.0.0-rc3',
     zip_safe=False,
 )
