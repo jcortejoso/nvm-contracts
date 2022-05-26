97e2ac1257a1cf890a2e7de2c010f4e20f2cacfa
Sami Mäkelä <mrsmkl@gmail.com>
2022-05-26 16:12:20 +0300

    create directory

--- scripts/keeper_entrypoint_nodeploy.sh
+++ scripts/keeper_entrypoint_nodeploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # We need to move the artifacts folder where it is expected and start openethereum
+mkdir -p /nevermined-contracts/artifacts
+mkdir -p /nevermined-contracts/circuits
 cp -rp /artifacts/* /nevermined-contracts/artifacts/
 cp -rp /circuits/* /nevermined-contracts/circuits/
 
