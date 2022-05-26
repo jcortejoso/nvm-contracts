fd2dac3a2ff7e77f2935d30814d3a563db43a673
Sami Mäkelä <mrsmkl@gmail.com>
2022-05-26 15:45:28 +0300

    trying to fix

--- scripts/keeper_entrypoint_nodeploy.sh
+++ scripts/keeper_entrypoint_nodeploy.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# We need to move the artifacts folder where it is expected and start openethereum
-cp -rp /nevermined-contracts/artifacts2/* /nevermined-contracts/artifacts/
-cp -rp /nevermined-contracts/circuits2/* /nevermined-contracts/circuits/
-
 exec /home/openethereum/openethereum "$@"
