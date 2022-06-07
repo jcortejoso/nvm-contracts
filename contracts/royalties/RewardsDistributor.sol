fafbf958ea7c1f21eb9ba2753a8e36379a20f22d
Sami Mäkelä <mrsmkl@gmail.com>
2022-06-07 12:16:53 +0300

    distributing ether rewards

--- contracts/royalties/RewardsDistributor.sol
+++ contracts/royalties/RewardsDistributor.sol
@@ -29,12 +29,28 @@ contract RewardsDistributor is Initializable {
         escrow = _escrow;
     }
 
-    // If receivers is changed, the unclaimed rewards will be divided only for first takers ...
+    /**
+     * @notice set receivers for did
+     * @param _did DID
+     * @param _addr list of receivers
+     */
     function setReceivers(bytes32 _did, address[] memory _addr) public {
         require(msg.sender == registry.getDIDCreator(_did), 'only creator can change');
         receivers[_did] = _addr;
     }
 
+    /**
+     * @notice distribute rewards associated with an escrow condition
+     * @dev as paramemeters, it just gets the same parameters as fulfill for escrow condition
+     * @param _agreementId agreement identifier
+     * @param _did asset decentralized identifier          
+     * @param _amounts token amounts to be locked/released
+     * @param _receivers receiver's address
+     * @param _lockPaymentAddress lock payment contract address
+     * @param _tokenAddress the ERC20 contract address to use during the payment
+     * @param _lockCondition lock condition identifier
+     * @param _releaseConditions release condition identifier
+     */
     function claimReward(
         bytes32 _agreementId,
         bytes32 _did,
@@ -70,14 +86,28 @@ contract RewardsDistributor is Initializable {
             }
         }
         uint256 receiversLen = receivers[_did].length;
-        for (uint256 i = 0; i < receiversLen; i++) {
-            if (i == 0) {
-                IERC20Upgradeable(_tokenAddress).safeTransfer(
-                    receivers[_did][i],
-                    rewardAmount - (rewardAmount / receiversLen) * (receiversLen-1)
-                );
-            } else {
-                IERC20Upgradeable(_tokenAddress).safeTransfer(receivers[_did][i], rewardAmount / receiversLen);
+        if (_tokenAddress == address(0)) {
+            for (uint256 i = 0; i < receiversLen; i++) {
+                if (i == 0) {
+                    bool sent;
+                    // solhint-disable-next-line                    
+                    (sent,) = receivers[_did][i].call{value: rewardAmount - (rewardAmount / receiversLen) * (receiversLen-1)}('');
+                } else {
+                    bool sent;
+                    // solhint-disable-next-line
+                    (sent,) = receivers[_did][i].call{value: rewardAmount / receiversLen}('');
+                }
+            }
+        } else {
+            for (uint256 i = 0; i < receiversLen; i++) {
+                if (i == 0) {
+                    IERC20Upgradeable(_tokenAddress).safeTransfer(
+                        receivers[_did][i],
+                        rewardAmount - (rewardAmount / receiversLen) * (receiversLen-1)
+                    );
+                } else {
+                    IERC20Upgradeable(_tokenAddress).safeTransfer(receivers[_did][i], rewardAmount / receiversLen);
+                }
             }
         }
     }
